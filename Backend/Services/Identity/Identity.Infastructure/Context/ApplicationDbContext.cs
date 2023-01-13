af8a466390cd620c5eb73e316be10aa64d4cba63
as005 <as005@DESKTOP-IEQKUIN>
2023-01-13 23:05:10 +0300

    Implement base user model, user configuration and db context

---
+++ Backend/Services/Identity/Identity.Infastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Identity.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Identity.Infastructure.Context
+{
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<int>, int>
+    {
+        public DbSet<User> Users { get; set; } = null!; //TODO here will be a bug, mb
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
