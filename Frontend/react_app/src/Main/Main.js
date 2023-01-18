f04ac3369f7612502049dbe1dc22e2f3dd216288
as005 <as005@DESKTOP-IEQKUIN>
2023-01-18 16:28:03 +0300

    Add flex and bootstrap in Main.js/css and add logo

--- Frontend/react_app/src/Main/Main.js
+++ Frontend/react_app/src/Main/Main.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import './Main.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function Main(){
     return (
-      <div className="Main-form-container">
-          <div className={"Elements-after-welcome-container"}>
-              <p className="Welcome-string">Welcome!</p>
-              <p className="Sentences-after-welcome">This web-application give you possibility to save your life`s tasks </p>
-              <div className="Vertical-line"/>
-              
+      <div className="container">
+          <div className="row">
+              <div className="col-sm">
+                  <div className={"Elements-after-welcome-container"}>
+                      <p className="Welcome-string">Welcome!</p>
+                      <p className="Sentences-after-welcome">
+                          This web-application give you possibility to save your life`s tasks
+                      </p>
+                      <div className="Vertical-line"/>
+                      <p className="Sentences-after-welcome">
+                          Even if you often forget something, you can save everything here and just see what you need to do!
+                      </p>
+                  </div>
+              </div>
+              <div className="col-sm">
+                  <img src={require('../Images/logo.png')} className='Logo-image'/>
+              </div>
           </div>
-
+          <div className='row Container-for-button'>
+              <button className="Button-for-login">Start an work with this application!</button>
+          </div>
       </div>
     );
 }
